28fb637e19ac0264773fd8d190bf8ce24b9a6f21
KaetiG <kaetigust@gmail.com>
2019-06-14 15:14:58 -0500

    add base code to Support component

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Feelings extends Component {
+class Understanding extends Component {
     render() {
         return (
             <>
@@ -9,4 +9,4 @@ class Feelings extends Component {
     }
 }
 
-export default Feelings;
+export default Understanding;
