76c3c94b2be37d25daf3e449c1ee3daf3c691108
KaetiG <kaetigust@gmail.com>
2019-06-14 15:02:16 -0500

    create all component folders and files-and base code for feelings component

---
+++ src/components/Feelings/Feelings.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Feelings extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default Feelings;
